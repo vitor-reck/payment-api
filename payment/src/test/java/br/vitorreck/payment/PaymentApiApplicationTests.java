f76822189b766333bc39febbd627c603e84bc219
vitor-reck <vitor_reck.150@hotmail.com>
2025-03-12 23:26:26 -0300

    First commit

---
+++ payment/src/test/java/br/vitorreck/payment/PaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.vitorreck.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
